6f39d25c97531e1e335bbd15fd971ab9b44c6cd5
Jonathan Mojoo <jonathanmojoo@yahoo.com>
2016-02-09 14:33:39 +0200

    About to pull from online repo

--- pull-snemis.bat
+++ pull-snemis.bat
@@ -2,8 +2,7 @@
 git add .
 git commit -m "About to pull from online repo"
 git pull snemis
-echo chancocs/&echo 20jmkm15 | git push snemis
-chancocs
-20jmkm15
+(echo chancocs
+echo 20jmkm15) | git push snemis
 sleep 61
 goto top
